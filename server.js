1c96cac15baaf0f7e18c00069f5a85ac74a704ed
وهيب <as@waheb.local>
2026-02-06 23:04:03 +0300

    first upload

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Webhook running ✅");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
